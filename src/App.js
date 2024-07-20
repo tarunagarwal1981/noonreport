cc168f71470199f3de214da61577eea8893a2c6e
tarunagarwal1981 <49944511+tarunagarwal1981@users.noreply.github.com>
2024-07-20 10:32:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AddVessel from './pages/AddVessel';
+import VesselList from './pages/VesselList';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/add-vessel" component={AddVessel} />
+        <Route path="/vessel-list" component={VesselList} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
 
